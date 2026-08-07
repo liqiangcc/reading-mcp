89f535a0961b074229e8968fbae2dca8b03d8734
Liqiangcc <43640935+liqiangcc@users.noreply.github.com>
2026-08-07 22:31:14 +0800

    feat: expose HTML parser

--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -1,8 +1,10 @@
 mod common;
+mod html;
 mod markdown;
 mod router;
 mod text;
 
+pub use html::HtmlParser;
 pub use markdown::MarkdownParser;
 pub use router::ParserRouter;
 pub use text::TextParser;

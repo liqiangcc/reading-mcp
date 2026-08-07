b0be217ffea4fc05bac29f801eb8ee5cf1625e27
Liqiangcc <43640935+liqiangcc@users.noreply.github.com>
2026-08-08 00:32:57 +0800

    feat: expose limited PDF parser

--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -1,11 +1,13 @@
 mod common;
 mod html;
+mod limited_pdf;
 mod markdown;
 mod pdf;
 mod router;
 mod text;
 
 pub use html::HtmlParser;
+pub use limited_pdf::LimitedPdfParser;
 pub use markdown::MarkdownParser;
 pub use pdf::PdfParser;
 pub use router::ParserRouter;

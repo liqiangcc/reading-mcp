c4fcfb28a195665bf7f87394e59b872a354fd555
Liqiangcc <43640935+liqiangcc@users.noreply.github.com>
2026-08-07 22:19:50 +0800

    feat: expose in-memory search index

--- src/infrastructure/mod.rs
+++ src/infrastructure/mod.rs
@@ -1,5 +1,7 @@
+mod memory_index;
 mod memory_repository;
 mod noop_index;
 
+pub use memory_index::InMemorySearchIndex;
 pub use memory_repository::InMemoryDocumentRepository;
 pub use noop_index::NoopSearchIndex;
